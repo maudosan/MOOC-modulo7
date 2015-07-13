d5fc2a1362784bb074ef243a9ac8869071b07448
maudosan <maurods16@gmail.com>
2015-07-13 18:40:36 +0200

    Primera pagina y favicon

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+/*
+Importar paquetes con middlewares
+*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,27 +9,33 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/*
+Importar enrutadores
+*/
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+// Crear aplicación
 var app = express();
 
-// view engine setup
+// view engine setup. Instalar generador de vistas EJS.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Instalar los middlewares importados
+app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Instalar los enrutadores importados
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
+// Instalar middleware para resto de rutas. Generará error 404 de HTTP.
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -36,6 +46,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+// Manejador de errores durante la fase de desarrollo.
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -48,6 +59,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+// Manejador de errores durante la fase de producción.
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
