4791550aa3768ff2014f2daf2714905243690d46
maudosan <maurods16@gmail.com>
2015-07-23 17:46:15 +0200

    Enlace a creditos

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,15 @@ var quizController= require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+	res.render('index', { title: 'Quiz' });
+});
+
+router.get('/author', function(req, res) {
+	res.render('author', { author: 'Mauro D.S.' });
 });
 
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
+
 
 module.exports = router;
