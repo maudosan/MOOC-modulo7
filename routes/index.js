cc4284732ce86b6310eaaeb083cedf4687ca6634
maudosan <maurods16@gmail.com>
2015-08-01 00:34:39 +0200

    Crear autoload

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 var quizController= require('../controllers/quiz_controller');
 
 /* GET home page. */
-
-
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+// Definición de rutas de /quizzes
 router.get('/quizzes', quizController.index);
 router.get('/quizzes/:quizId(\\d+)', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
