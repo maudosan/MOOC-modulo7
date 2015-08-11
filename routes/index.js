05811b47ec4b4ae16674da3f695905565df97343
maudosan <maurods16@gmail.com>
2015-08-11 19:45:49 +0200

    Autorización de usuarios y un pequeño error en un archivo

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,11 @@ router.get('/logout', sessionController.destroy); // destruir sesión
 router.get('/quizzes', quizController.index);
 router.get('/quizzes/:quizId(\\d+)', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizzes/new', quizController.new);
-router.post('/quizzes/create', quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizzes/:quizId(\\d+)', quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',     quizController.destroy);
+router.get('/quizzes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizzes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios.
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
